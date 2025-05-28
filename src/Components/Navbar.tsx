724e1cefeb2ef4152c29e54dbf80ed58cbe5b8f4
fer0o <fer0osennin@gmail.com>
2025-05-27 23:50:06 -0600

    eliminando errores de produccion

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import { motion, hover, animate } from 'motion/react'
+import { motion} from 'motion/react'
 import { SocialIcon } from 'react-social-icons'
 import { socialsMockData } from '@/services/socialMockData'
 import { Sun, Moon } from 'lucide-react'
