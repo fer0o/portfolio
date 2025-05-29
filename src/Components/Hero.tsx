39a85bdcd1655f8f246e27c9075e4eaf842f74d8
fer0o <fer0osennin@gmail.com>
2025-05-29 16:07:56 -0600

    cambiando el image por Image componente de Next y se le dio una mejor apariencia para mobiles

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,4 +1,4 @@
-// import Image from 'next/image'
+import Image from 'next/image'
 import React from 'react'
 import { useTypewriter } from 'react-simple-typewriter'
 
@@ -18,18 +18,23 @@ const Hero: React.FC<Props> = ({ darkMode }: Props) => {
     typeSpeed: 80,
     deleteSpeed: 80
   })
+  const handleClickButton = () => {
+    console.log(`Button clicked!`)
+  }
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 items-center justify-center gap-2 border-2 border-white p-2 rounded-md shadow-lg '>
-      <div className='flex flex-col items-center justify-center  space-y-4 h-full md:order-1 order-2 md:px-16 px-8'>
+    <div className='grid grid-cols-1 md:grid-cols-2 items-center justify-center gap-2 border-2 border-white p-2 mt-12 md:mt-0 rounded-md shadow-lg'>
+      <div className='flex flex-col items-center justify-center space-y-4 h-full md:order-1 order-2 md:px-16 px-8'>
+        {/* saludo con el nombre */}
         <div className=''>
           <h1
-            className={`text-lg md:text-3xl font-bold transition-colors duration-500 ${
+            className={`text-lg md:text-3xl font-bold transition-colors duration-500 text-wrap ${
               darkMode ? 'text-yellow-400' : 'text-blue-800'
             }`}
           >
             Hola Soy Fernando Medellin
           </h1>
         </div>
+        {/* typewriting */}
         <div className='flex items-center justify-center p-8 max-h-16 w-96 overflow-hidden'>
           <span
             className={`text-lg md:text-2xl font-bold transition-colors duration-500 ${
@@ -39,43 +44,48 @@ const Hero: React.FC<Props> = ({ darkMode }: Props) => {
             {text}
           </span>
         </div>
-        <div className='grid md:grid-rows-1 grid-cols-2 md:grid-cols-4 gap-2'>
+        {/* grid de los botones */}
+        <div className='grid md:grid-rows-1 grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-2'>
           <button
-            className={`px-6 py-2 font-semibold rounded-full border transition-all duration-300 ease-in-out
+            onClick={handleClickButton}
+            className={`p-2 font-semibold rounded-full border transition-all duration-300 ease-in-out
     ${
       darkMode
-        ? 'bg-white text-black border-black shadow-sky-500 hover:shadow-[2px_5px_0_0_white] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_white]'
-        : 'bg-white text-black border-black shadow-black hover:shadow-[2px_5px_0_0_black] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_black]'
+        ? 'bg-white text-black border-black shadow-sky-500 hover:shadow-[2px_5px_0_0_white] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_white] text-wrap text-xs md:text-sm'
+        : 'bg-white text-black border-black shadow-black hover:shadow-[2px_5px_0_0_black] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_black] text-xs md:text-sm text-wrap'
     }`}
           >
             Experiencia
           </button>
           <button
-            className={`px-6 py-2 font-semibold rounded-full border transition-all duration-300 ease-in-out
+            onClick={handleClickButton}
+            className={`p-2 font-semibold rounded-full border transition-all duration-300 ease-in-out
     ${
       darkMode
-        ? 'bg-white text-black border-black shadow-sky-500 hover:shadow-[2px_5px_0_0_white] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_white]'
-        : 'bg-white text-black border-black shadow-black hover:shadow-[2px_5px_0_0_black] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_black]'
+        ? 'bg-white text-black border-black shadow-sky-500 hover:shadow-[2px_5px_0_0_white] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_white] text-xs md:text-sm text-wrap'
+        : 'bg-white text-black border-black shadow-black hover:shadow-[2px_5px_0_0_black] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_black] text-xs md:text-sm text-wrap'
     }`}
           >
             Experiencia
           </button>
           <button
-            className={`px-6 py-2 font-semibold rounded-full border transition-all duration-300 ease-in-out
+            onClick={handleClickButton}
+            className={`p-2 font-semibold rounded-full border transition-all duration-300 ease-in-out
     ${
       darkMode
-        ? 'bg-white text-black border-black shadow-sky-500 hover:shadow-[2px_5px_0_0_white] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_white]'
-        : 'bg-white text-black border-black shadow-black hover:shadow-[2px_5px_0_0_black] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_black]'
+        ? 'bg-white text-black border-black shadow-sky-500 hover:shadow-[2px_5px_0_0_white] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_white] text-xs md:text-sm text-wrap'
+        : 'bg-white text-black border-black shadow-black hover:shadow-[2px_5px_0_0_black] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_black] text-xs md:text-sm text-wrap'
     }`}
           >
             Experiencia
           </button>
           <button
-            className={`px-6 py-2 font-semibold rounded-full border transition-all duration-300 ease-in-out
+            onClick={handleClickButton}
+            className={`p-2 font-semibold rounded-full border transition-all duration-300 ease-in-out
     ${
       darkMode
-        ? 'bg-white text-black border-black shadow-sky-500 hover:shadow-[2px_5px_0_0_white] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_white]'
-        : 'bg-white text-black border-black shadow-black hover:shadow-[2px_5px_0_0_black] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_black]'
+        ? 'bg-white text-black border-black shadow-sky-500 hover:shadow-[2px_5px_0_0_white] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_white] text-xs md:text-sm text-wrap'
+        : 'bg-white text-black border-black shadow-black hover:shadow-[2px_5px_0_0_black] hover:-translate-y-1 hover:-translate-x-0.5 active:translate-y-0.5 active:translate-x-0.5 active:shadow-[0_0_0_0_black] text-xs md:text-sm text-wrap'
     }`}
           >
             Experiencia
@@ -83,20 +93,14 @@ const Hero: React.FC<Props> = ({ darkMode }: Props) => {
         </div>
       </div>
       <div className='flex items-center justify-center h-full md:order-2 order-1 '>
-        {/* <Image
-          src={'/Photo/Fer.jpg'}
-          alt='Foto de perfil'
-          width={400}
-          height={500}
-          className='object-cover'
-        /> */}
-        <img
-          loading='lazy'
+        <Image
           src='/Photo/Fer.jpg'
           alt='Foto de perfil'
-          className='object-cover rounded-md w-64 h-72 md:w-96 md:h-full shadow-lg object-cover'
           width={400}
           height={500}
+          className='object-cover rounded-md  w-64 h-72 md:w-96 md:h-full shadow-lg'
+          priority={false}
+          loading='lazy'
         />
       </div>
     </div>
